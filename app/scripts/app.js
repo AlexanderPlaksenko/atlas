731e50c3812e998052a174ffb1764d4b6db3b718
a.plaksin <a.plaksin@synergetica.co>
2017-02-23 16:32:06 +0300

    wip

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,3 +4,10 @@ import $ from 'jquery';
 $(() => {
 	svg4everybody();
 });
+var $document = $(document),
+	$element = $('.fixed-button'),
+	className = 'hasScrolled';
+
+$document.scroll(function() {
+	$element.toggleClass(className, $document.scrollTop() >= 50);
+});
